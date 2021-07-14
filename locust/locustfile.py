c4cdd5a963d9216be471cbc48c06a55fca3511ea
Jeffrey Koehler <deef551@gmail.com>
2021-07-14 03:03:11 -0500

    dockerile can do full, to websocket, run of pollster.

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,39 +1,118 @@
+#!/usr/bin/python
+
 import random
-from locust import HttpUser, task, between
-from uuid import uuid4
+import gevent
 import urllib3
+import ssl
+import time
+
+
+from locust import HttpUser, task, between, events
+from websocket import create_connection
+
+from uuid import uuid4
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
+host = "192.168.100.153:4201"
+print("Default host: %s" % (host))
 class QuickstartUser(HttpUser):
 	wait_time = between(1,1)
-	host = "https://192.168.100.153:4201/polls/3"
+#get poll
+#get vote
+#post vote
+#get poll
+#get vote
+#get results
+	def on_start(self):
+		self.wsLoop = True
+		print("test")
+		if not self.host:
+			self.host = host
+			print("setting host to %s" % self.host)
+
+	def on_stop(self):
+		print("closing WS. %s" % self.id)
+		self.wsLoop = False
+		time.sleep(1)
+		self.ws.close()
 
 	@task
-	def getPoll(self):
+	def userFlow(self):
+		print("loop status: %s" % self.wsLoop)
 		id = uuid4().hex
-		Poll = 3
-		u = "https://192.168.100.153:4201/polls/3"
-		self.client.get(url=u,
-			headers={"subject-uuid": id, "Cookie": "sub="+id},
-			verify=False,
-		)
+		self.id = id
+		poll = 3
+		self.httpEndpoint = "https://" + self.host + "/polls/" + str(poll)
+		self.wsEndpoint = "wss://" + self.host + "/polls/" + str(poll)
+		getPoll(self, id, poll)
+		getVote(self, id, poll)
+		postVote(self, id, poll)
+		getPoll(self, id, poll)
+		getVote(self, id, poll)
+		getResults(self, id, poll)
 
-	# @task
-	def vote(self):
-		id = uuid4().hex
-		#print("User ID (%s) Generated" % self)
-		# r = random.randint(1, 5)
-		Items = [ random.randint(0,1) ]
-		Poll = 3
-		#Poll = random.randint(3,103)
-		d = {"Items": Items, "Poll": 3}
-		u = "https://192.168.100.153:4201/polls/3/v"
-		print("Data: ", u , d)
-		self.client.post(
-			url=u,
-			json={"Items": Items, "Poll": 3},
-			headers={"subject-uuid": id, "Cookie": "sub="+id},
-			verify=False,
-		)
+
+def getPoll(self, id, poll):
+	dest = self.httpEndpoint
+
+	print("getPoll: (%d)" % poll)
+	self.client.get(url=dest,
+		headers={"subject-uuid": id},
+		verify=False,
+	)
+
+def getVote(self, id, poll):
+	dest = self.httpEndpoint + "/v"
+
+	print("getVote: (%d)" % poll)
+	self.client.get(
+		url=dest,
+		headers={"subject-uuid": id},
+		verify=False,
+	)
+
+def postVote(self, id, poll):
+	Items = [ random.randint(0,1) ]
+	voteData = {"Items": Items, "Poll": poll}
+	dest = self.httpEndpoint + "/v"
+
+	print("postVote: (%d), (%s)" % (poll, voteData))
+	self.client.post(
+		url=dest,
+		json=voteData,
+		headers={"subject-uuid": id},
+		verify=False,
+	)
+
+def getResults(self, id, poll):
+
+	dest = self.wsEndpoint + "/r"
+	print("wsResults: (%d)" % poll)
+	ws = create_connection(dest, 
+		sslopt={"cert_reqs": ssl.CERT_NONE},
+		header={"subject-uuid": id, "Cookie": "dev_sub="+id}
+	)
+	self.ws = ws
+	# TODO should actually be adding these to an array, and closing ALL of them.
+
+	def _receive():
+		while True:
+			res = ws.recv()
+			# data = json.loads(res)
+			events.request_success.fire(
+				request_type='WebSocket Recv',
+				name="polls/" + str(poll)+"/results",
+				response_time=0,
+				response_length=len(res),
+			)
+			ws.send("PONG")
+			print("data back (%s) %s" % (id, res))
+
+		print("done watching data.")
+
+	
+	_receive()
+	# gevent.spawn(_receive)
 
